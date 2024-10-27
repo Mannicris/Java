637c5e78dfbc5f3ff9afa2b8adb1d8d6d4a9934e
Mannicris <CM>
2024-10-27 19:10:27 +0100

    Format code

--- Codewars/src/helloworld/HelloWorld.java
+++ Codewars/src/helloworld/HelloWorld.java
@@ -1,36 +1,35 @@
 package helloworld;
 
 public class HelloWorld {
-	public static void main(String[] args)
-	{
+	public static void main(String[] args) {
 		int n = oddCount(3);
 		System.out.println(n);
 	}
-	
+
 	public static String duckDuckGoose(Player[] players, int goose) {
-	    // Note: Player objects have a String field called 'name'.
+		// Note: Player objects have a String field called 'name'.
 		return players[(goose % players.length) - 1].name;
-	  }
-	
+	}
+
 	class Player {
 		public String name;
 	}
-	
-	public static int oddCount(int n){
-		return n/2;
-	  }
-	
-	public static String doubleChar(String s) {
-		String result="";
-		for (char c : s.toCharArray()) {
-			result+= c;
-			result+= c;
-		}
-		
-		return result; 
+
+	public static int oddCount(int n) {
+		return n / 2;
 	}
-	
+
+	public static String doubleChar(String s) {
+		String result = "";
+		for (char c : s.toCharArray()) {
+			result += c;
+			result += c;
+		}
+
+		return result;
+	}
+
 	public static boolean solution(String str, String ending) {
-	    return str.endsWith(ending);
-	  }
+		return str.endsWith(ending);
+	}
 }
