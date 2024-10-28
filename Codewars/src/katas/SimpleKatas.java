ccda84f5f0e4da416f18aaf1fabc068a63c6dbfb
Mannicris <CM>
2024-10-28 12:25:55 +0100

    Renamed HelloWorld.java to SimpleKatas.java

--- Codewars/src/katas/SimpleKatas.java
+++ Codewars/src/katas/SimpleKatas.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 import java.util.HashSet;
 import java.util.stream.Collectors;
 
-public class HelloWorld {
+public class SimpleKatas {
 	static int count = 1;
 
 	public static void main(String[] args) {
